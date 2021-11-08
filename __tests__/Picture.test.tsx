6f048a760e69ea61a68229af541318fb89b9e3da
Arimario Jesus <arimario.jesus@hotmail.com>
2021-11-07 21:03:21 -0300

    test: add test to check if tags have correct className

--- __tests__/Picture.test.tsx
+++ __tests__/Picture.test.tsx
@@ -13,12 +13,26 @@ describe('<Picture />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('should be able to show the picture element', () => {
+  it('should be able to show the picture tag and img tag', () => {
     render(
       <Picture src="https://via.placeholder.com/768x432" alt="Placeholder" />,
     );
 
+    const pictureElement = screen.getByTestId('react-picture-ratio');
+    expect(pictureElement).toBeInTheDocument();
+
+    const imgElement = screen.getByAltText('Placeholder');
+    expect(imgElement).toBeInTheDocument();
+  });
+
+  it('picture tag and img tag should have correct className', () => {
+    render(
+      <Picture src="https://via.placeholder.com/768x432" alt="Placeholder" />,
+    );
     const picture = screen.getByTestId('react-picture-ratio');
-    expect(picture).toBeInTheDocument();
+    const image = picture.firstChild;
+
+    expect(picture).toHaveClass('ratioBox');
+    expect(image).toHaveClass('image');
   });
 });
